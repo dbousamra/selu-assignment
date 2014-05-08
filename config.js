95abd7ec5e4d64464adb1e522d28fc4bac78121d
Dominic Bou-Samra <dominic.bou.samra@gmail.com>
2014-05-08 10:21:14 +1000

    Pushed the database configuration logic into a config.js file

---
+++ config.js
@@ -0,0 +1,7 @@
+module.exports = {
+ db: {
+   production: "mongodb://user:pass@example.com:1234/selu-assignment-prod",
+   development: "mongodb://localhost/selu-assignment-dev",
+   test: "mongodb://localhost/selu-assignment-test",
+ }
+};
